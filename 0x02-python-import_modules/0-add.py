29f88d23ff7c55a5058a3b51afad0508e0f932cc
Thabisa <tab95mazingisa@gmail.com>
2023-08-11 00:11:31 -0700

    0-add

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+from 0-add import add
+
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
+if __name__ == "__main__":
+    main()
