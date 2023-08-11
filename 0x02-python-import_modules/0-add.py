e1b2c18538b551463cb651b95df780858ae163b7
Thabisa <tab95mazingisa@gmail.com>
2023-08-11 01:52:27 -0700

    import module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,11 +3,10 @@ def add(a, b):
     return a + b
 from add_0 import add
 
+if __name__ == "__main__":
+    main()
 def main():
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    main()
+     print("{} + {} = {}".format(a, b, add(a, b)))
 
